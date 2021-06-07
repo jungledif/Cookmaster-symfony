d55fd31c7700093eee132f968844e4bee24c65f5
jungledif <denis.benjamin.pro@gmail.com>
2021-06-07 11:02:57 +0200

    Recipe entity

---
+++ migrations/Version20210607085826.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210607085826 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE recipe (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, descriptive LONGTEXT DEFAULT NULL, type VARCHAR(255) NOT NULL, level VARCHAR(255) NOT NULL, step1 VARCHAR(800) NOT NULL, step2 LONGTEXT NOT NULL, step3 LONGTEXT NOT NULL, recipe_img VARCHAR(255) DEFAULT NULL, cooktime VARCHAR(255) NOT NULL, servings NUMERIC(10, 0) NOT NULL, creation_date VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE recipe');
+    }
+}
---
+++ src/Entity/Recipe.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RecipeRepository::class)
+ */
+class Recipe
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $descriptive;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $level;
+
+    /**
+     * @ORM\Column(type="string", length=800)
+     */
+    private $step1;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $step2;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $step3;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $recipe_img;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cooktime;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $servings;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $creationDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescriptive(): ?string
+    {
+        return $this->descriptive;
+    }
+
+    public function setDescriptive(?string $descriptive): self
+    {
+        $this->descriptive = $descriptive;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getLevel(): ?string
+    {
+        return $this->level;
+    }
+
+    public function setLevel(string $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getStep1(): ?string
+    {
+        return $this->step1;
+    }
+
+    public function setStep1(string $step1): self
+    {
+        $this->step1 = $step1;
+
+        return $this;
+    }
+
+    public function getStep2(): ?string
+    {
+        return $this->step2;
+    }
+
+    public function setStep2(string $step2): self
+    {
+        $this->step2 = $step2;
+
+        return $this;
+    }
+
+    public function getStep3(): ?string
+    {
+        return $this->step3;
+    }
+
+    public function setStep3(string $step3): self
+    {
+        $this->step3 = $step3;
+
+        return $this;
+    }
+
+    public function getRecipeImg(): ?string
+    {
+        return $this->recipe_img;
+    }
+
+    public function setRecipeImg(?string $recipe_img): self
+    {
+        $this->recipe_img = $recipe_img;
+
+        return $this;
+    }
+
+    public function getCooktime(): ?string
+    {
+        return $this->cooktime;
+    }
+
+    public function setCooktime(string $cooktime): self
+    {
+        $this->cooktime = $cooktime;
+
+        return $this;
+    }
+
+    public function getServings(): ?string
+    {
+        return $this->servings;
+    }
+
+    public function setServings(string $servings): self
+    {
+        $this->servings = $servings;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?string
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(?string $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+}
